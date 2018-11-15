fbdc31345b0d2d7507fbd2d866e2aee1d64733cd
Justin Lam <justin.lam@rea-group.com>
2018-11-15 13:16:05 +1100

    Complete exerciseState

--- exerciseState/src/main/scala/scan/Scanner.scala
+++ exerciseState/src/main/scala/scan/Scanner.scala
@@ -4,106 +4,130 @@ import java.nio.file._
 
 import scala.compat.java8.StreamConverters._
 import scala.collection.SortedSet
-
 import cats._
 import cats.data._
 import cats.implicits._
-
 import mouse.all._
-
 import org.atnos.eff._
 import org.atnos.eff.all._
 import org.atnos.eff.syntax.all._
-
 import org.atnos.eff.addon.monix._
 import org.atnos.eff.addon.monix.task._
 import org.atnos.eff.syntax.addon.monix.task._
-
 import monix.eval._
 import monix.execution._
-
 import EffTypes._
 
 import scala.concurrent.duration._
 
-
 object Scanner {
   val Usage = "Scanner <path> [number of largest files to track]"
 
-  type R = Fx.fx5[Task, Reader[Filesystem, ?], Either[String, ?], Writer[Log, ?], State[Set[FilePath], ?]]
+  type R =
+    Fx.fx5[
+      Task,
+      Reader[Filesystem, ?],
+      Either[String, ?],
+      Writer[Log, ?],
+      State[Set[FilePath], ?]
+    ]
 
   implicit val s = Scheduler(ExecutionModel.BatchedExecution(32))
 
   def main(args: Array[String]): Unit = {
-    val program = scanReport[R](args).map(println)
+    val program: Eff[R, Unit] = scanReport[R](args).map(println)
 
-    program.runReader(DefaultFilesystem: Filesystem).evalStateZero[Set[FilePath]].runEither.runWriterUnsafe[Log]{
-      case Error(msg) => System.err.println(msg)
-      case Info(msg) => System.out.println(msg)
-      case _ => ()
-    }.runAsync.runSyncUnsafe(1.minute)
+    program
+      .runReader(DefaultFilesystem: Filesystem)
+      .evalStateZero[Set[FilePath]]
+      .runEither
+      .runWriterUnsafe[Log] {
+        case Error(msg) => System.err.println(msg)
+        case Info(msg)  => System.out.println(msg)
+        case _          => ()
+      }
+      .runAsync
+      .runSyncUnsafe(1.minute)
   }
 
-  def scanReport[R: _task: _filesystem: _err: _log](args: Array[String]): Eff[R, String] = for {
-    base <- optionEither(args.lift(0), s"Path to scan must be specified.\n$Usage")
+  def scanReport[R: _task: _filesystem: _err: _log: _sym](args: Array[String]): Eff[R, String] =
+    for {
+      base <- optionEither(args.lift(0), s"Path to scan must be specified.\n$Usage")
 
-    topN <- {
-      val n = args.lift(1).getOrElse("10")
-      fromEither(n.parseInt.leftMap(_ => s"Number of files must be numeric: $n"))
+      topN <- {
+        val n = args.lift(1).getOrElse("10")
+        fromEither(n.parseInt.leftMap(_ => s"Number of files must be numeric: $n"))
+      }
+      topNValid <- if (topN < 0) left[R, String, Int](s"Invalid number of files $topN")
+      else topN.pureEff[R]
+
+      fs <- ask
+
+      start <- taskDelay(System.currentTimeMillis())
+
+      scan <- pathScan[Fx.prepend[Reader[ScanConfig, ?], R]](fs.filePath(base))
+        .runReader[ScanConfig](ScanConfig(topNValid))
+
+      finish <- taskDelay(System.currentTimeMillis())
+
+      _ <- tell(Log.info(s"Scan of $base completed in ${finish - start}ms"))
+
+    } yield ReportFormat.largeFilesReport(scan, base.toString)
+
+  def pathScan[R: _task: _filesystem: _config: _log: _sym](path: FilePath): Eff[R, PathScan] =
+    path match {
+      case Symlink(_,linkTo) =>
+        for {
+          linksVisited <- get[R, Set[FilePath]]
+          scan <- if (!linksVisited.contains(linkTo)) {
+            // we could also use modify which takes a function to modify the state
+            // the >> is an operator (alias) for flatmap(_ => fb)
+            put[R, Set[FilePath]](linksVisited + linkTo) >> pathScan(linkTo)
+          } else {
+            PathScan.empty.pureEff[R]
+          }
+        } yield scan
+
+      case f: File =>
+        for {
+          fs <- FileSize.ofFile(f)
+          _ <- tell(
+            Log.debug(s"File ${fs.file.path} Size ${ReportFormat.formatByteString(fs.size)}")
+          )
+        } yield PathScan(SortedSet(fs), fs.size, 1)
+
+      case dir: Directory =>
+        for {
+          filesystem <- ask[R, Filesystem]
+          topN <- takeTopN
+          fileList <- taskDelay(filesystem.listFiles(dir))
+          childScans <- fileList.traverse(pathScan(_))
+          _ <- {
+            val dirCount = fileList.count(_.isInstanceOf[Directory])
+            val fileCount = fileList.count(_.isInstanceOf[File])
+            tell(
+              Log.debug(s"Scanning directory '$dir': $dirCount subdirectories and $fileCount files")
+            )
+          }
+        } yield childScans.combineAll(topN)
+
+      case Other(_) =>
+        PathScan.empty.pureEff
     }
-    topNValid <- if (topN < 0) left[R, String, Int](s"Invalid number of files $topN") else topN.pureEff[R]
 
-    fs <- ask
+  def takeTopN[R: _config]: Eff[R, Monoid[PathScan]] =
+    for {
+      scanConfig <- ask
+    } yield
+      new Monoid[PathScan] {
+        def empty: PathScan = PathScan.empty
 
-    start <- taskDelay(System.currentTimeMillis())
-
-    scan <- pathScan[Fx.prepend[Reader[ScanConfig, ?], R]](
-      fs.filePath(base)).runReader[ScanConfig](ScanConfig(topNValid))
-
-    finish <- taskDelay(System.currentTimeMillis())
-
-    _ <- tell(Log.info(s"Scan of $base completed in ${finish - start}ms"))
-
-  } yield ReportFormat.largeFilesReport(scan, base.toString)
-
-  def pathScan[R: _task: _filesystem: _config: _log](path: FilePath): Eff[R, PathScan] = path match {
-
-    case f: File =>
-      for {
-        fs <- FileSize.ofFile(f)
-        _ <- tell(Log.debug(s"File ${fs.file.path} Size ${ReportFormat.formatByteString(fs.size)}"))
-      } yield PathScan(SortedSet(fs), fs.size, 1)
-
-    case dir: Directory =>
-      for {
-        filesystem <- ask[R, Filesystem]
-        topN <- takeTopN
-        fileList <- taskDelay(filesystem.listFiles(dir))
-        childScans <- fileList.traverse(pathScan(_))
-        _ <- {
-          val dirCount = fileList.count(_.isInstanceOf[Directory])
-          val fileCount = fileList.count(_.isInstanceOf[File])
-          tell(Log.debug(s"Scanning directory '$dir': $dirCount subdirectories and $fileCount files"))
-        }
-      } yield childScans.combineAll(topN)
-
-    case Other(_) =>
-      PathScan.empty.pureEff
-  }
-
-
-
-  def takeTopN[R: _config]: Eff[R, Monoid[PathScan]] = for {
-    scanConfig <- ask
-  } yield new Monoid[PathScan] {
-    def empty: PathScan = PathScan.empty
-
-    def combine(p1: PathScan, p2: PathScan): PathScan = PathScan(
-      p1.largestFiles.union(p2.largestFiles).take(scanConfig.topN),
-      p1.totalSize + p2.totalSize,
-      p1.totalCount + p2.totalCount
-    )
-  }
+        def combine(p1: PathScan, p2: PathScan): PathScan = PathScan(
+          p1.largestFiles.union(p2.largestFiles).take(scanConfig.topN),
+          p1.totalSize + p2.totalSize,
+          p1.totalCount + p2.totalCount
+        )
+      }
 
 }
 
@@ -116,6 +140,7 @@ trait Filesystem {
   def listFiles(directory: Directory): List[FilePath]
 
 }
+
 case object DefaultFilesystem extends Filesystem {
 
   def filePath(pathStr: String): FilePath = {
@@ -140,10 +165,10 @@ case object DefaultFilesystem extends Filesystem {
 
   private def listFilePath(path: String): List[FilePath] = {
     filePath(path) match {
-      case Directory(path) => List(Directory(path))
-      case File(path) => List(File(path))
+      case Directory(path)   => List(Directory(path))
+      case File(path)        => List(File(path))
       case Symlink(path, to) => listFilePath(to.path)
-      case Other(path) => List.empty
+      case Other(path)       => List.empty
     }
   }
 }
@@ -172,23 +197,24 @@ case class FileSize(file: File, size: Long)
 
 object FileSize {
 
-  def ofFile[R: _filesystem](file: File): Eff[R, FileSize] = for {
-    fs <- ask
-  } yield  FileSize(file, fs.length(file))
+  def ofFile[R: _filesystem](file: File): Eff[R, FileSize] =
+    for {
+      fs <- ask
+    } yield FileSize(file, fs.length(file))
 
   implicit val ordering: Ordering[FileSize] = Ordering.by[FileSize, Long](_.size).reverse
 
 }
 
 object EffTypes {
-
   type _filesystem[R] = Reader[Filesystem, ?] <= R
   type _config[R] = Reader[ScanConfig, ?] <= R
   type _err[R] = Either[String, ?] <= R
   type _log[R] = Writer[Log, ?] <= R
+  type _sym[R] = State[Set[FilePath], ?] <= R
 }
 
-sealed trait Log {def msg: String}
+sealed trait Log { def msg: String }
 object Log {
   def error: String => Log = Error(_)
   def info: String => Log = Info(_)
@@ -216,10 +242,13 @@ object ReportFormat {
   def largeFilesReport(scan: PathScan, rootDir: String): String = {
     if (scan.largestFiles.nonEmpty) {
       s"Largest ${scan.largestFiles.size} file(s) found under path: $rootDir\n" +
-        scan.largestFiles.map(fs => s"${(fs.size * 100)/scan.totalSize}%  ${formatByteString(fs.size)}  ${fs.file}").mkString("", "\n", "\n") +
+        scan.largestFiles
+          .map(
+            fs => s"${(fs.size * 100) / scan.totalSize}%  ${formatByteString(fs.size)}  ${fs.file}"
+          )
+          .mkString("", "\n", "\n") +
         s"${scan.totalCount} total files found, having total size ${formatByteString(scan.totalSize)} bytes.\n"
-    }
-    else
+    } else
       s"No files found under path: $rootDir"
   }
 
